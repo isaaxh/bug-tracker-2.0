011739231aa35646f6ee133deab25952f59465d3
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-12 19:48:11 +0300

    add more colors

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,22 +1,31 @@
+import { useState } from "react";
 import style from "./Login.module.css";
 import LanguageIcon from "@mui/icons-material/Language";
+import InfoIcon from "@mui/icons-material/Info";
 
 const Login = () => {
+  const [error, setError] = useState<string>(
+    "We don't recognize this sign in combination"
+  );
   return (
     <div className={style.container}>
       <div className={style.header}>
-        <h1>BugTracker 2.0</h1>
+        <h1 className={style.title}>BugTracker 2.0</h1>
         <h1>
           <LanguageIcon fontSize='large' />
         </h1>
       </div>
       <div className={style.body}>
         <div className={style.card}>
-          <h1 className={style.title}>Sign In</h1>
+          <h1 className={style["card-title"]}>Sign In</h1>
+          <div className={style["error-card"]}>
+            <InfoIcon style={{ color: "var(--color-error)" }} />
+            {error}
+          </div>
           <form className={style.form} onSubmit={() => {}}>
             <div className={style["input-container"]}>
               <label htmlFor='email'>Email</label>
-              <input className={style["input-field"]} type='text' id='email' />
+              <input className={style["input-field"]} type='email' id='email' />
             </div>
             <div className={style["input-container"]}>
               <label htmlFor='password'>Password</label>
