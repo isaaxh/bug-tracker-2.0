5dabc163bf02a7d00f0d1556ee1b8bd69bf3991b
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-13 16:39:24 +0300

    factor: remove unnecessary code

--- src/ProtectedRoutes.tsx
+++ src/ProtectedRoutes.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import SignIn from "./components/auth/signin/SignIn";
 import { useAuth } from "./hooks/useAuth";
 import { useEffect, useState } from "react";
