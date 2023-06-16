129894da9b2422fc0323f502e11f2c1d2aaac895
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-16 07:39:24 +0300

    add basic element to the drop down menu for user actions

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,18 +1,97 @@
 import { Avatar } from "@mui/material";
 import img1 from "../../../assets/man-smiling.jpg";
 import style from "./header.module.css";
+import { Logout } from "@mui/icons-material";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import { Link } from "react-router-dom";
+
+const subMenuLinks = [
+  {
+    title: "Edit Profile",
+    className: "user-info-links",
+    path: "/profile",
+    icon: (
+      <PersonOutlinedIcon
+        className={style.icons}
+        sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+        fontSize='large'
+      />
+    ),
+  },
+  {
+    title: "Settings",
+    className: "user-info-links",
+    path: "/",
+    icon: (
+      <SettingsOutlinedIcon
+        className={style.icons}
+        sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+        fontSize='large'
+      />
+    ),
+  },
+  {
+    title: "Log out",
+    className: "user-info-links",
+    path: "",
+    icon: (
+      <Logout
+        className={style.icons}
+        sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+        fontSize='large'
+      />
+    ),
+  },
+];
 
 const Header = () => {
   return (
     <div className={style.container}>
-      <div className={style.header}>
+      <nav className={style.header}>
         <div className={style["logo-container"]}>
           <h1 className='app-title'>Bug Tracker 2.0</h1>
         </div>
-        <div className={style["avatar-container"]}>
-          <Avatar alt='man smiling' src={img1} sx={{ width: 60, height: 60 }} />
+        <div className='menu-wrapper'>
+          <div className={style["avatar-container"]}>
+            <Avatar
+              alt='man smiling'
+              src={img1}
+              sx={{ width: 60, height: 60 }}
+            />
+          </div>
+          <div className={style["sub-menu-wrap"]}>
+            <div className={style["sub-menu"]}>
+              <div className={style["user-info"]}>
+                <Avatar
+                  className={style["user-info-avatar"]}
+                  alt='man smiling'
+                  src={img1}
+                  sx={{ width: 40, height: 40 }}
+                />
+                <h2>Isaac Hussain</h2>
+              </div>
+              <hr />
+              {subMenuLinks.map((link, index) => (
+                <Link to={link.path} key={index}>
+                  <div className={style["user-info-links"]}>
+                    <div className={style["icon-container"]}>{link.icon}</div>
+                    <p>{link.title}</p>
+                    <span>
+                      <KeyboardArrowRightOutlinedIcon
+                        className={style["right-arrows"]}
+                        sx={{ stroke: "#ffffff", strokeWidth: 0.5 }}
+                        fontSize='large'
+                      />
+                    </span>
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
   );
 };
