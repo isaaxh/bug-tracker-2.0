0a1e6fffab66d1ccfd8c5f8c316bef36e699b497
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-18 06:43:51 +0300

    add select tag and required elements for it

--- src/components/roleAssignment/RoleAssignment.tsx
+++ src/components/roleAssignment/RoleAssignment.tsx
@@ -1,16 +1,80 @@
+import { useState } from "react";
 import style from "./roleAssignment.module.css";
 
+// interface usersData {}
+
 const RoleAssignment = () => {
+  const [users, setUsers] = useState([
+    {
+      name: "tom hidleston",
+    },
+    {
+      name: "John smith",
+    },
+    {
+      name: "Carol smith",
+    },
+    {
+      name: "Carol smith",
+    },
+    {
+      name: "Carol smith",
+    },
+    {
+      name: "Carol smith",
+    },
+    {
+      name: "Carol smith",
+    },
+    {
+      name: "Carol smith",
+    },
+    {
+      name: "Carol smith",
+    },
+    {
+      name: "Carol smith",
+    },
+    {
+      name: "Carol smith",
+    },
+  ]);
+
+  // const handleSelectChange = (e) => {
+  //   let options = e.target.options;
+  // };
+
   return (
     <div className={style.container}>
       <div className={style["title-container"]}>
         <h1 className={style.title}>Manage User Roles</h1>
       </div>
       <div className={style["content-container"]}>
-        <div className={style["users-list-container"]}>List of users here</div>
-        <div className={style["select-role-container"]}>
-          Select role to assign here
+        <div className={style["select-users-container"]}>
+          <form action='#'>
+            <div className='select-users'>
+              <label className={style["user-list-label"]} htmlFor='user-list'>
+                Select 1 or more users
+              </label>
+              <select
+                className={style["user-list"]}
+                name='user-list'
+                id='user-list'
+                multiple
+                size={3}
+                required
+                autoFocus
+              >
+                {users.map((user) => (
+                  <option className={style["users-items"]}>{user.name}</option>
+                ))}
+              </select>
+            </div>
+            <div className='select-role'>Select role to assign here</div>
+          </form>
         </div>
+        {/* <div className={style["select-role-container"]}> */}
+        {/* </div> */}
         <div className={style["Peronnel-list-container"]}>
           list of personnels here
         </div>
