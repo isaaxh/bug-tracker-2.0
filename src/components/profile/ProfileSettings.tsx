bc013674d56ad9c9d67d1bc8a970624536c97414
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-19 10:03:01 +0300

    add grid functionality to profile settings layout

--- src/components/profile/ProfileSettings.tsx
+++ src/components/profile/ProfileSettings.tsx
@@ -3,28 +3,40 @@ import style from "./profileSettings.module.css";
 const ProfileSettings = () => {
   return (
     <div className={style.container}>
-      <div>
-        <h3 className='item-titles'>Full name</h3>
-        <p>isaac hussain</p>
-        <button>Edit</button>
-      </div>
-      <div>
-        <h3 className='item-titles'>Email</h3>
-        <p>isaaclearns@mail.com</p>
-        <button>Edit</button>
-      </div>
-      <div>
-        <h3 className='item-titles'>Password settings</h3>
-        <p>0000000</p>
-        <button>Reset</button>
-      </div>
-      <div>
-        <h3 className='item-titles'>Profile Image</h3>
-        <p>Edit</p>
-      </div>
-      <div>
-        <h3 className='item-titles'>Notifications settings</h3>
-        <button>enable / disable</button>
+      <div className={style["grid-wrapper"]}>
+        <div className={style["grid-item"]}>
+          <div className={style["text-container"]}>
+            <h3 className='item-titles'>Full name</h3>
+            <p>isaac hussain</p>
+          </div>
+          <button>Edit</button>
+        </div>
+        <div className={style["grid-item"]}>
+          <div className={style["text-container"]}>
+            <h3 className='item-titles'>Email</h3>
+            <p>isaaclearns@mail.com</p>
+          </div>
+          <button>Edit</button>
+        </div>
+        <div className={style["grid-item"]}>
+          <div className={style["text-container"]}>
+            <h3 className='item-titles'>Password settings</h3>
+            <p>0000000</p>
+          </div>
+          <button>Reset</button>
+        </div>
+        <div className={style["grid-item"]}>
+          <div className={style["text-container"]}>
+            <h3 className='item-titles'>Profile Image</h3>
+            <p>Edit</p>
+          </div>
+        </div>
+        <div className={style["grid-item"]}>
+          <div className={style["text-container"]}>
+            <h3 className='item-titles'>Notifications settings</h3>
+            <button>enable / disable</button>
+          </div>
+        </div>
       </div>
     </div>
   );
