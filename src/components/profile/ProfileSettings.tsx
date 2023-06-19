6ea790ca350996f7dc4ba089a9493027939b8664
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-19 09:49:04 +0300

    add htm structure to profileSettings page

--- src/components/profile/ProfileSettings.tsx
+++ src/components/profile/ProfileSettings.tsx
@@ -1,7 +1,33 @@
 import style from "./profileSettings.module.css";
 
 const ProfileSettings = () => {
-  return <div>Profile Settings</div>;
+  return (
+    <div className={style.container}>
+      <div>
+        <h3 className='item-titles'>Full name</h3>
+        <p>isaac hussain</p>
+        <button>Edit</button>
+      </div>
+      <div>
+        <h3 className='item-titles'>Email</h3>
+        <p>isaaclearns@mail.com</p>
+        <button>Edit</button>
+      </div>
+      <div>
+        <h3 className='item-titles'>Password settings</h3>
+        <p>0000000</p>
+        <button>Reset</button>
+      </div>
+      <div>
+        <h3 className='item-titles'>Profile Image</h3>
+        <p>Edit</p>
+      </div>
+      <div>
+        <h3 className='item-titles'>Notifications settings</h3>
+        <button>enable / disable</button>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileSettings;
