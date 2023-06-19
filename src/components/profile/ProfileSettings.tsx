a80cc5fe0f340461c1fc352630b6654fae9b5eb8
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-19 10:11:22 +0300

    add code optimization to profile settings page

--- src/components/profile/ProfileSettings.tsx
+++ src/components/profile/ProfileSettings.tsx
@@ -1,42 +1,46 @@
 import style from "./profileSettings.module.css";
 
+const settings = [
+  {
+    title: "Full Name",
+    content: "Isaac Hussain",
+    buttonTitle: "Edit",
+  },
+  {
+    title: "Email",
+    content: "isaac@email.com",
+    buttonTitle: "Edit",
+  },
+  {
+    title: "Password Settings",
+    content: "000000000",
+    buttonTitle: "Reset",
+  },
+  {
+    title: "Profile Image",
+    content: "",
+    buttonTitle: "Edit",
+  },
+  {
+    title: "Notification Settings",
+    content: "",
+    btnTitle: "Enable",
+  },
+];
+
 const ProfileSettings = () => {
   return (
     <div className={style.container}>
       <div className={style["grid-wrapper"]}>
-        <div className={style["grid-item"]}>
-          <div className={style["text-container"]}>
-            <h3 className='item-titles'>Full name</h3>
-            <p>isaac hussain</p>
+        {settings.map((setting, index) => (
+          <div className={style["grid-items"]} key={index}>
+            <div className={style["text-container"]}>
+              <h3 className={style["item-titles"]}>{setting.title}</h3>
+              <p>{setting.content}</p>
+            </div>
+            <button className={style.btn}>{setting.btnTitle}</button>
           </div>
-          <button>Edit</button>
-        </div>
-        <div className={style["grid-item"]}>
-          <div className={style["text-container"]}>
-            <h3 className='item-titles'>Email</h3>
-            <p>isaaclearns@mail.com</p>
-          </div>
-          <button>Edit</button>
-        </div>
-        <div className={style["grid-item"]}>
-          <div className={style["text-container"]}>
-            <h3 className='item-titles'>Password settings</h3>
-            <p>0000000</p>
-          </div>
-          <button>Reset</button>
-        </div>
-        <div className={style["grid-item"]}>
-          <div className={style["text-container"]}>
-            <h3 className='item-titles'>Profile Image</h3>
-            <p>Edit</p>
-          </div>
-        </div>
-        <div className={style["grid-item"]}>
-          <div className={style["text-container"]}>
-            <h3 className='item-titles'>Notifications settings</h3>
-            <button>enable / disable</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
