a00c84900395b2cec6014ea9ee78e9389d1543f3
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-20 07:23:06 +0300

    add hover effect on sidebar links

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+// import useAuth from "../hooks/useAuth";
+
+// interface AuthProviderPropsType {}
+
+// const AuthContext = () => {
+//   const [currentUser, setCurrentUser] = useState(null);
+
+//   const { currentUser } = useAuth();
+
+//   return <div>AuthContext</div>;
+// };
+
+// export default AuthContext;
