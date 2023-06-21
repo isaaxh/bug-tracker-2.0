b5d3c9dc536d20a3dda8e55de1e2a97dc4dc53f3
Isaac Hussain <isaaclearns58@gmail.com>
2023-06-21 14:43:34 +0300

    fix

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext } from "react";
 import useAuth from "../hooks/useAuth";
 import { User } from "firebase/auth";
 
@@ -10,25 +10,14 @@ export interface AuthContextType {
   currentUser: User | null;
 }
 
-interface AuthValues {
-  currentUser: User;
-}
-
 export const AuthContext = createContext<AuthContextType | null>(null);
 
 const AuthProvider = ({ children }: AuthProviderPropsType) => {
   const { currentUser } = useAuth();
 
-  let AuthValues: AuthValues | null = null;
-
-  if (currentUser !== null) {
-    AuthValues = {
-      currentUser,
-    };
-  } else {
-    console.log("Current User Unavailable");
-  }
-
+  const AuthValues = {
+    currentUser,
+  };
   return (
     <AuthContext.Provider value={AuthValues}>{children}</AuthContext.Provider>
   );
