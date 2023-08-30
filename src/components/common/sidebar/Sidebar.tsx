cb813b7298eeb504cebd3a882c88a5ae7db20860
Isaac Hussain <isaaclearns58@gmail.com>
2023-08-31 00:49:18 +0300

    conditionally render tabs based on userDataRoles

--- src/components/common/sidebar/Sidebar.tsx
+++ src/components/common/sidebar/Sidebar.tsx
@@ -6,8 +6,10 @@ import ConfirmationNumberOutlinedIcon from "@mui/icons-material/ConfirmationNumb
 import GroupAddOutlinedIcon from "@mui/icons-material/GroupAddOutlined";
 import PeopleOutlineOutlinedIcon from "@mui/icons-material/PeopleOutlineOutlined";
 import useAuth from "../../../hooks/useAuth";
-import { useEffect } from "react";
 import { AuthContextType } from "../../../contexts/AuthContext";
+import { useEffect, useState } from "react";
+import MoonLoader from "react-spinners/MoonLoader";
+
 
 const sidebarLinks = [
   {
@@ -67,25 +69,52 @@ const sidebarLinks = [
   },
 ];
 
+
 const Sidebar = () => {
     const { currentUserData } = useAuth() as AuthContextType;
-   
-   useEffect(() => {
-        console.log(currentUserData)
+    const [allowedLinks, setAllowedLinks] = useState(sidebarLinks);
+    const [loading, setLoading] = useState(true)
+
+    const filterAllowedRoles = () => {
+
+        const filteredLinks = sidebarLinks.filter((link) => {
+                if (currentUserData?.roles?.admin) return true;
+                if (currentUserData?.roles?.manager && link.path !== '/role_assignment') return true;
+                if (currentUserData?.roles?.developer && (link.path === '/' || link.path === '/tickets')) return true;
+            })
+
+        setLoading(false)
+        return filteredLinks
+    }
+
+    useEffect(() => {
+        setAllowedLinks(filterAllowedRoles())
     }, [])
+
     
+   if (loading) return (
+        <div style={{display: 'grid', placeItems: 'start', padding: 100}}>
+            <MoonLoader
+                className={style.spinner}
+                loading={loading}
+                aria-label='Loading Spinner'
+                data-testid='loader'
+                size={30}
+            />
+        </div>
+        );
   return (
     <nav className={style.container}>
       <ul className={style["tab-list"]}>
-        {sidebarLinks.map((link, index) => (
-          <li className={`${style.links}`} key={index}>
+        {allowedLinks.map((link) => (
+          <li className={`${style.links}`} key={link.path}>
             <NavLink to={link.path}>
               <div className={style["icon-container"]}>{link.icon}</div>
               {link.title}
             </NavLink>
           </li>
         ))}
-      </ul>
+      </ul> 
     </nav>
   );
 };
