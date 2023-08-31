110f841c22ae01e7702772af55d6d9914fab7a8c
Isaac Hussain <isaaclearns58@gmail.com>
2023-08-31 07:39:26 +0300

    saving...

--- src/components/roleAssignment/RoleAssignment.tsx
+++ src/components/roleAssignment/RoleAssignment.tsx
@@ -2,170 +2,67 @@ import { useEffect, useState } from "react";
 import style from "./roleAssignment.module.css";
 import useFirestore from "../../hooks/useFirestore";
 import {
-  readAllDocsPropType,
-  readMultipleDocsPropsType,
+    readAllDocsPropType,
+    readMultipleDocsPropsType,
 } from "../../hooks/useFirestore";
 import { DocumentData } from "firebase/firestore";
-import MoonLoader from "react-spinners/MoonLoader";
 import RoleTable from "./RoleTable";
-import { userDataType } from "../../contexts/AuthContext";
 import useWindowDimensions from "../../hooks/useWindowDimensions";
+import SelectUser from "./SelectUser";
 
-const roles = ["admin", "manager", "developer"];
 
 const RoleAssignment = () => {
-  const [selectedUsers, setSelectedUsers] = useState<Array<string>>([]);
-  // const [selectedRole, setSelectedRole] = useState<Array<string>>([]);
-  const [allUserDocs, setAllUserDocs] = useState<DocumentData>([]);
-  const [unAssignedUsers, setUnAssignedUsers] = useState<DocumentData>([]);
-  const { width } = useWindowDimensions();
+    const [selectedUsers, setSelectedUsers] = useState<Array<string>>([]);
+    // const [selectedRole, setSelectedRole] = useState<Array<string>>([]);
+    const [allUserDocs, setAllUserDocs] = useState<DocumentData>([]);
+    const [unAssignedUsers, setUnAssignedUsers] = useState<DocumentData>([]);
+    const { width } = useWindowDimensions();
 
 
-  const { readAllDocs, readMultipleDocs, error, loading } = useFirestore();
+    const { readAllDocs, readMultipleDocs, error, loading } = useFirestore();
 
-  useEffect(() => {
-    const allDocsQuery: readAllDocsPropType = {
-      collectionName: "users",
-    };
+    useEffect(() => {
+        const allDocsQuery: readAllDocsPropType = {
+            collectionName: "users",
+        };
 
-    const multipleDocsQuery: readMultipleDocsPropsType = {
-      collectionName: "users",
-      queryObject: {
-        field: "roleAssigned",
-        operator: "==",
-        value: false,
-      },
-    };
+        const multipleDocsQuery: readMultipleDocsPropsType = {
+            collectionName: "users",
+            queryObject: {
+                field: "roleAssigned",
+                operator: "==",
+                value: false,
+            },
+        };
 
-    const fetchAllData = async () => {
-      const allUserDocs = await readAllDocs(allDocsQuery);
-      setAllUserDocs(allUserDocs);
+        const fetchAllData = async () => {
+            const allUserDocs = await readAllDocs(allDocsQuery);
+            setAllUserDocs(allUserDocs);
 
-      const unAssignedUsers = await readMultipleDocs(multipleDocsQuery);
-      setUnAssignedUsers(unAssignedUsers);
-    };
+            const unAssignedUsers = await readMultipleDocs(multipleDocsQuery);
+            setUnAssignedUsers(unAssignedUsers);
+        };
 
-    fetchAllData();
-  }, []);
+        fetchAllData();
+    }, []);
 
-  useEffect(() => {
-    console.log(selectedUsers);
-  }, [selectedUsers]);
+    useEffect(() => {
+        console.log(selectedUsers);
+    }, [selectedUsers]);
 
-  const handleUsersSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedValues = Array.from(
-      e.target.selectedOptions,
-      (option) => option.value
+
+    return (
+        <div className={style.container}>
+            <div className={style["title-container"]}>
+                {width > 1200 ?
+                    <h1 className={style.title}>Manage User Roles</h1> : null}
+            </div>
+            <div className={style["content-container"]}>
+                <SelectUser unAssignedUsers={unAssignedUsers} loading={loading} selectedUsers={selectedUsers} />
+                <RoleTable allUserDocs={allUserDocs} loading={loading} />
+            </div>
+        </div>
     );
-
-    console.log(selectedValues);
-
-    setSelectedUsers(selectedValues);
-  };
-
-  const handleRoleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedValues = e.target.options;
-    // Array.from(  e.target.selectedOptions,(option) => option.value);
-
-    console.log(e.target);
-
-    // setSelectedRole(selectedValues);
-  };
-
-  const handleMultiUsersClick = (value: string) => {
-    if (selectedUsers.includes(value)) {
-      setSelectedUsers(selectedUsers.filter((user) => user != value));
-    } else {
-      setSelectedUsers([...selectedUsers, value]);
-    }
-  };
-
-  const handleSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
-
-  return (
-    <div className={style.container}>
-      <div className={style["title-container"]}>
-        {width > 1200 ? 
-            <h1 className={style.title}>Manage User Roles</h1>
-            : null}
-      </div>
-      <div className={style["content-container"]}>
-        <div className={style["input-container"]}>
-          <form action='#' onSubmit={handleSubmitForm} className={style.form}>
-            <div className={style["select-user-container"]}>
-              <label className={style["user-list-label"]} htmlFor='user-list'>
-                Select 1 or more unassigned users
-              </label>
-              {loading && loading ? (
-                <MoonLoader
-                  className={style.spinner}
-                  loading={loading}
-                  aria-label='Loading Spinner'
-                  data-testid='loader'
-                  size={20}
-                />
-              ) : (
-                <select
-                  className={style["user-list"]}
-                  name='user-list'
-                  id='user-list'
-                  multiple
-                  size={3}
-                  required
-                  autoFocus
-                  onChange={handleUsersSelectChange}
-                >
-                  {unAssignedUsers.map((userData: userDataType, index: number) => (
-                    <option
-                      className={style["select-items"]}
-                      key={index}
-                      value={userData.displayName}
-                    >
-                      {userData.displayName}
-                    </option>
-                  ))}
-                </select>
-              )}
-              <div>CTRL + Select multiple users</div>
-            </div>
-            <hr className={style.hr} />
-            <div className={style["select-role-container"]}>
-              <label className={style["user-list-label"]} htmlFor='role-list'>
-                Select the Role to assign
-              </label>
-              <select
-                className={style["role-list"]}
-                name='role-list'
-                id='role-list'
-                required
-                autoFocus
-                onChange={handleRoleSelectChange}
-              >
-                <option disabled value=''>
-                  --Choose one please--
-                </option>
-                {roles.map((role, index) => (
-                  <option
-                    className={style["select-items"]}
-                    key={index}
-                    value={role}
-                  >
-                    {role}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <button className={style["btn-submit"]}>Submit</button>
-          </form>
-        </div>
-        <div className={style["output-container"]}>
-          <RoleTable allUserDocs={allUserDocs} loading={loading} />
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default RoleAssignment;
--- src/components/roleAssignment/RoleTable.tsx
+++ src/components/roleAssignment/RoleTable.tsx
@@ -5,67 +5,70 @@ import { useEffect } from "react";
 import { userDataType } from "../../contexts/AuthContext";
 
 interface RoleTablePropsTypes {
-  allUserDocs: DocumentData;
-  loading: boolean;
+    allUserDocs: DocumentData;
+    loading: boolean;
 }
 
 const RoleTable = ({ allUserDocs, loading }: RoleTablePropsTypes) => {
-  useEffect(() => {
-    allUserDocs && console.log(allUserDocs);
-  }, [allUserDocs]);
+    useEffect(() => {
+        allUserDocs && console.log(allUserDocs);
+    }, [allUserDocs]);
 
-  const getRole = (userData: DocumentData) => {
-    if (userData.roles.admin) {
-      return "admin";
-    } else if (userData.roles.manager) {
-      return "manager";
-    } else if (userData.roles.developer) {
-      return "developer";
-    } else {
-        return 'unassigned';
+    const getRole = (userData: DocumentData) => {
+        if (userData.roles.admin) {
+            return "admin";
+        } else if (userData.roles.manager) {
+            return "manager";
+        } else if (userData.roles.developer) {
+            return "developer";
+        } else {
+            return 'unassigned';
+        }
     }
-  }
 
 
-  return (
-    <div className={style["table-wrapper"]}>
-      {/* <div className='label-container'>
+    return (
+
+        <div className={style["output-container"]}>
+            <div className={style["table-wrapper"]}>
+                {/* <div className='label-container'>
               <h2>Your Personnel</h2>
               <p>All users in your database</p>
             </div> */}
 
-      {loading && loading ? (
-        <MoonLoader
-          className={style.spinner}
-          loading={loading}
-          aria-label='Loading Spinner'
-          data-testid='loader'
-          size={30}
-        />
-      ) : (
-        <table>
-          <caption>Your Personnel</caption>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Role</th>
-            </tr>
-          </thead>
+                {loading && loading ? (
+                    <MoonLoader
+                        className={style.spinner}
+                        loading={loading}
+                        aria-label='Loading Spinner'
+                        data-testid='loader'
+                        size={30}
+                    />
+                ) : (
+                    <table>
+                        <caption>Your Personnel</caption>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                            </tr>
+                        </thead>
 
-          <tbody>
-            {allUserDocs.map((userData: userDataType, index: number) => (
-              <tr key={index}>
-                <td data-cell='name'>{userData.displayName}</td>
-                <td data-cell='email'>{userData.email}</td>
-                <td data-cell='role'>{getRole(userData)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
-  );
+                        <tbody>
+                            {allUserDocs.map((userData: userDataType, index: number) => (
+                                <tr key={index}>
+                                    <td data-cell='name'>{userData.displayName}</td>
+                                    <td data-cell='email'>{userData.email}</td>
+                                    <td data-cell='role'>{getRole(userData)}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default RoleTable;
