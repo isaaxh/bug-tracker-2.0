0970348105d15a82f7df1a4bd23681edf2bab0a9
Isaac Hussain <isaaclearns58@gmail.com>
2023-09-01 04:56:40 +0300

    change clickedBtn var name to modalTitle

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -1,9 +1,6 @@
-import { useContext } from "react";
 import ClearIcon from "@mui/icons-material/Clear";
-import {
-  GlobalContext,
-  GlobalContextType,
-} from "../../../contexts/GlobalContext";
+import { GlobalContextType } from "../../../contexts/GlobalContext";
+import useGlobal from "../../../hooks/useGlobal.ts"
 import useFirestore from "../../../hooks/useFirestore";
 import InfoIcon from "@mui/icons-material/Info";
 // import { AuthContext, AuthContextType } from "../../../contexts/AuthContext";
@@ -11,47 +8,45 @@ import ModalFullName from "./ModalFullName";
 import ModalProfilePic from "./ModalProfileImg";
 
 const Modal = () => {
-  const { modalOpen, toggleModalOpen, clickedBtn } = useContext(
-    GlobalContext
-  ) as GlobalContextType;
+    const { modalOpen, toggleModalOpen, modalTitle } = useGlobal() as GlobalContextType;
 
-  const { error } = useFirestore();
+    const { error } = useFirestore();
 
-  return (
-    <div
-      className={modalOpen ? "modal-overlay" : "modal-closed"}
-      onClick={toggleModalOpen}
-    >
-      <div className='modal' onClick={(e) => e.stopPropagation()}>
-        <div className='modal-header'>
-          <h2 className='modal-title'>{clickedBtn}</h2>
-          <span onClick={toggleModalOpen}>
-            <ClearIcon
-              className='modal-cross'
-              sx={{ stroke: "#ffffff", strokeWidth: 1 }}
-              fontSize='large'
-            />
-          </span>
+    return (
+        <div
+            className={modalOpen ? "modal-overlay" : "modal-closed"}
+            onClick={toggleModalOpen}
+        >
+            <div className='modal' onClick={(e) => e.stopPropagation()}>
+                <div className='modal-header'>
+                    <h2 className='modal-title'>{modalTitle}</h2>
+                    <span onClick={toggleModalOpen}>
+                        <ClearIcon
+                            className='modal-cross'
+                            sx={{ stroke: "#ffffff", strokeWidth: 1 }}
+                            fontSize='large'
+                        />
+                    </span>
+                </div>
+                {error && error !== "" ? (
+                    <div className='error-box'>
+                        <InfoIcon
+                            fontSize='large'
+                            style={{ color: "var(--color-error)" }}
+                        />
+                        {error}
+                    </div>
+                ) : null}
+                <>
+                    {modalTitle === "Edit Full Name" ? (
+                        <ModalFullName />
+                    ) : modalTitle === "Edit Profile Photo" ? (
+                        <ModalProfilePic />
+                    ) : null}
+                </>
+            </div>
         </div>
-        {error && error !== "" ? (
-          <div className='error-box'>
-            <InfoIcon
-              fontSize='large'
-              style={{ color: "var(--color-error)" }}
-            />
-            {error}
-          </div>
-        ) : null}
-        <>
-          {clickedBtn === "Edit Full Name" ? (
-            <ModalFullName />
-          ) : clickedBtn === "Edit Profile Photo" ? (
-            <ModalProfilePic />
-          ) : null}
-        </>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Modal;
