f757afa073289caa058a42f203617b03aba12082
Isaac Hussain <isaaclearns58@gmail.com>
2023-09-25 15:08:32 +0300

    add search bar styling to user table in role-assignment

--- src/components/roleAssignment/RoleTable.tsx
+++ src/components/roleAssignment/RoleTable.tsx
@@ -1,13 +1,24 @@
 import style from "./roleAssignment.module.css";
 import { DocumentData } from "firebase/firestore";
 import MoonLoader from "react-spinners/MoonLoader";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { userDataType } from "../../contexts/AuthContext";
 
 interface RoleTablePropsTypes {
   allUserDocs: DocumentData;
   loading: boolean;
 }
+
+interface SortDataProps {
+  tableData: userDataType[];
+  sortKey: SortKeysType;
+  reverse: SortOrderType;
+}
+
+type SortKeysType = "name" | "email" | "role" | "date";
+type SortOrderType = "asc" | "desc";
+
+function sortData({ tableData, sortKey, reverse }: SortDataProps) {}
 
 const RoleTable = ({ allUserDocs, loading }: RoleTablePropsTypes) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -16,6 +27,8 @@ const RoleTable = ({ allUserDocs, loading }: RoleTablePropsTypes) => {
   const firstIndex = lastIndex - recordsPerPage;
   const records = allUserDocs.slice(firstIndex, lastIndex);
   const totalPages = Math.ceil(allUserDocs.length / recordsPerPage);
+  const [sortKeys, setSortKeys] = useState<SortKeysType>("name");
+  const [sortOrder, setOrder] = useState<SortOrderType>("asc");
 
   const getRole = (userData: DocumentData) => {
     if (userData.roles.admin) {
@@ -28,6 +41,12 @@ const RoleTable = ({ allUserDocs, loading }: RoleTablePropsTypes) => {
       return "submitter";
     }
   };
+
+  /* const sortedData = useCallback( */
+  /*   () => */
+  /*     sortData({ tableData: data, sortKeys, reverse: sortOrder === "desc" }), */
+  /*   [data, sortKeys, sortOrder], */
+  /* ); */
 
   const prevPage = () => {
     if (currentPage !== 1) {
@@ -52,32 +71,41 @@ const RoleTable = ({ allUserDocs, loading }: RoleTablePropsTypes) => {
             size={30}
           />
         ) : (
-          <table>
-            <caption>Your Personnel</caption>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Date Added</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {records.map((userData: userDataType, index: number) => (
-                <tr key={index}>
-                  <td data-cell="name">{userData.displayName}</td>
-                  <td data-cell="email">{userData.email}</td>
-                  <td data-cell="role">{getRole(userData)}</td>
-                  <td data-cell="date added">{userData.createdAt}</td>
+          <div>
+            <div className={style["table-header"]}>
+              <h2>All Users</h2>
+              <div className={style["search-container"]}>
+                <input placeholder="Search users" />
+                <button type="button">Search</button>
+              </div>
+            </div>
+            <table>
+              {/* <caption>Users</caption> */}
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Date</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+
+              <tbody>
+                {records.map((userData: userDataType, index: number) => (
+                  <tr key={index}>
+                    <td data-cell="name">{userData.displayName}</td>
+                    <td data-cell="email">{userData.email}</td>
+                    <td data-cell="role">{getRole(userData)}</td>
+                    <td data-cell="date">{userData.createdAt}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         )}
         <nav className={style["pagination-wrapper"]}>
           <div className={style["page-numbers"]}>
-            {currentPage} of {totalPages} pages.
+            {currentPage} of {totalPages} {totalPages > 1 ? "pages" : "page"}.
           </div>
           <ul className={style["table-pagination"]}>
             <li className={style["page-item"]}>
